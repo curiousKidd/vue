0f039e9e5deeb7d3df197b69d4172cb277f1aae6
curiousKidd <skasrjhaan@gmail.com>
2022-09-26 22:58:34 +0900

    20220926(월) - vue component add

--- vue_nuxt/layouts/default.vue
+++ vue_nuxt/layouts/default.vue
@@ -81,6 +81,11 @@ export default {
           title: 'Inspire',
           to: '/inspire',
         },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'test',
+          to: '/socket',
+        },
       ],
       miniVariant: false,
       right: true,
--- vue_nuxt/pages/index.vue
+++ vue_nuxt/pages/index.vue
@@ -74,6 +74,7 @@
         <v-card-actions>
           <v-spacer />
           <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+          <v-btn color="primary" nuxt to="/socket"> test </v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
